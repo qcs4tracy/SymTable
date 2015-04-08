3d646df1ccd523ed3896bd98abd0bdc7172511a4
QiuChusheng <ChushengQiu@QiuChushengdeMac-mini.local>
2015-04-08 11:46:10 -0500

    Initial Commit

---
+++ SymTable/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SymTable
+//
+//  Created by QiuChusheng on 15/4/8.
+//  Copyright (c) 2015年 QiuChusheng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
